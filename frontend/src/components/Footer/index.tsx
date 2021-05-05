95f2cf0a525b210583ce91ab97aa37833f6019cc
Diego Luis <diego_bezerra95@hotmail.com>
2021-05-04 21:18:49 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {//COMPONENTE DE RODA PE
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/diego-luis-a8486b1b8/" target="_blank" rel="noreferrer">Diego Luis</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
