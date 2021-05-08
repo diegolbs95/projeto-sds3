93c99f811bd8be7727d834478c34800be46b25ef
Diego Luis <diego_bezerra95@hotmail.com>
2021-05-08 18:49:04 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,13 @@
+import {BrowserRouter} from 'react-router-dom';
+const Routes = () => {
+    return (
+        <div>
+            <BrowserRouter>
+            </BrowserRouter>
+        </div>
+        
+
+    );
+}
+
+export default Routes;
